92d106d2d1622833b2b5ce08787a552e9e25f472
raaynaldo <raaynaldo@hotmail.com>
2020-10-13 16:40:55 -0400

    Add BloadOath's class.

---
+++ app/models/bload_oath.rb
@@ -0,0 +1,13 @@
+require "date"
+
+class BloadOath
+  attr_reader :cult, :follower, :initiation_date
+  @@all = []
+
+  def initialize(cult, follower)
+    @cult = cult
+    @follower = follower
+    @initiation_date = DateTime.now.strftime("%F")
+    @@all << self
+  end
+end
