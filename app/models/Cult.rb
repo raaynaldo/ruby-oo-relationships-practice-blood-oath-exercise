8666cbf065f3a4ba6975b692ea238c0751b402dc
raaynaldo <raaynaldo@hotmail.com>
2020-10-13 23:18:11 -0400

    Advance Methods - Analytics

--- app/models/Cult.rb
+++ app/models/Cult.rb
@@ -20,6 +20,23 @@ class Cult
     BloadOath.all.count { |bo| bo.cult == self }
   end
 
+  def followers
+    BloadOath.all
+      .select { |bo| bo.cult == self }
+      .map { |bo_cult| bo_cult.follower }
+  end
+
+  def average_age
+    followers_ages = self.followers
+      .map { |follower| follower.age }
+
+    followers_ages.inject { |sum, item| sum + item }.to_f / followers_ages.count
+  end
+
+  def my_followers_mottos
+    self.followers.each { |follower| puts follower.life_motto }
+  end
+
   def self.all
     @@all
   end
@@ -34,5 +51,14 @@ class Cult
 
   def self.find_by_founding_year(year)
     self.all.select { |cult| cult.founding_year == year }
+  end
+
+  def self.least_popular
+    self.all.min_by {|cult| cult.cult_population}
+  end
+
+  def self.most_common_location
+    cult_locations = self.all.map{|cult| cult.location}
+    cult_locations.max_by {|i| cult_locations.count(i)}
   end
 end
--- app/models/follower.rb
+++ app/models/follower.rb
@@ -11,12 +11,16 @@ class Follower
 
   def cults
     # returns an Array of this follower's cults
-    BloadOath.all.select{|bo| bo.follower == self}.map{|bo| bo.cult}
+    BloadOath.all.select { |bo| bo.follower == self }.map { |bo| bo.cult }
   end
 
   def join_cult(cult)
     # takes in an argument of a Cult instance and adds this follower to the cult's list of followers
     BloadOath.new(cult, self)
+  end
+
+  def my_cults_slogans
+    cults.each { |cult| puts cult.slogan }
   end
 
   def self.all
@@ -25,5 +29,13 @@ class Follower
 
   def self.of_a_certain_age(age)
     self.all.select { |follower| follower.age == age }
+  end
+
+  def self.most_active
+    self.all.max_by { |follower| follower.cults.count() }
+  end
+
+  def self.top_ten
+    self.all.sort_by { |follower| -follower.cults.count() }.take(10)
   end
 end
